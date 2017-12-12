fb452b871e2fca4bcb13ebb2ef380fa79d20c5bc
JaiminWu <jaiminwu@163.com>
2017-12-12 16:26:45 +0800

    Seeding topics and users data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +16,17 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     static $password;
+    $now = Carbon::now()->toDateTimeString();
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        //表达式 (expr1) ? (expr2) : (expr3) 在 expr1 求值为 TRUE 时的值为 expr2，在 expr1 求值为 FALSE 时的值为 expr3。
+        //自 PHP 5.3 起，可以省略三元运算符中间那部分。表达式 expr1 ?: expr3 在 expr1 求值为 TRUE 时返回 expr1，否则返回 expr3。
+        'password' => $password ?: $password = bcrypt('password'),
         'remember_token' => str_random(10),
+        'introduction' => $faker->sentence(),
+        'created_at' => $now,
+        'updated_at' => $now,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+                        ->times(10)
+                        ->make()
+                        //这一步相当于foreach，$user就是$value，$index就是$key
+                        //use 是 PHP 中匿名函数提供的本地变量传递机制，匿名函数中必须通过 use 声明的引用，才能使用本地变量。
+                        ->each(function ($user, $index)
+                            use ($faker, $avatars)
+        {
+            // 从头像数组中随机取出一个并赋值
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Jaimin';
+        $user->email = 'JaiminWu@tmgworldwide.com';
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+    }
+}
