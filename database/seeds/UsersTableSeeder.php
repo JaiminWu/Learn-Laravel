d6305f75c4729f907d7514495d6bcfbcebf2483d
JaiminWu <jaiminwu@163.com>
2018-01-08 15:10:42 +0800

    Notify user in email with Queue supported

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -42,7 +42,7 @@ class UsersTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'Jaimin';
-        $user->email = 'JaiminWu@tmgworldwide.com';
+        $user->email = '223804239@qq.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
     }
